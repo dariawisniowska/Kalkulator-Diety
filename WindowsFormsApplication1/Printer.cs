3d9569dda97ba55c6fc234b4dc0bb9e07b41f111
Daria Wiśniowska <daria.wisniowska123@gmail.com>
2018-11-27 21:46:46 +0100

    Marginesy w pliku dekadówki

--- WindowsFormsApplication1/Printer.cs
+++ WindowsFormsApplication1/Printer.cs
@@ -993,13 +993,13 @@ namespace WindowsFormsApplication1
                         using (DocX document = DocX.Create(path))
                         {
                             document.PageLayout.Orientation = Xceed.Words.NET.Orientation.Landscape;
-                            document.MarginTop = 5;
+                            document.MarginTop = 10;
                             document.MarginHeader = 0;
-                            document.MarginBottom = 5;
+                            document.MarginBottom = 10;
                             document.MarginFooter = 0;
                             Paragraph p = document.InsertParagraph();
                             p.Alignment = Alignment.center;
-                            p.Append("Od " + dataOd + " do " + dataDo + ", " + d.nazwa + "\r\n")
+                            p.Append("Od " + dataOd + " do " + dataDo + ", " + d.nazwa )
                             .Font("Times New Roman")
                             .FontSize(12)
                             .Color(Color.Black)
